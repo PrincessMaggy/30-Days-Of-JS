86d1349892c740d587ae2710711297d94e4fca18
PrincessMaggy <edoziemagdalene@gmail.com>
2021-08-18 23:48:14 -0700

    Completed Day1 Task

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # 30-Days-Of-JS
+
+#  TASK 1- </br>
+* Write a program that logs your bio data to the console. </br>
+Note: Each bio data should be stored in it's own variable and use naming conventions. </br>
+What is variable scope in javascript and how is let different from var? 
---
+++ Task 1/index.js
@@ -0,0 +1,11 @@
+function bioData(){
+let name = 'Edozie Magdalene';
+let nickname ='Maggy';
+let age = '24 years old';
+let stateOfOrigin = 'Delta State';
+let gender= 'Female';
+let nationality= 'Nigerian';
+let maritalStatus= 'Single';
+console.log([name , nickname, stateOfOrigin, age , gender, nationality, maritalStatus]);
+}
+bioData();
