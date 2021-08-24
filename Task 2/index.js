1d8ff298d82333439e298b9b3c89a6724518a9fc
PrincessMaggy <edoziemagdalene@gmail.com>
2021-08-23 19:20:24 -0700

    made some changes

--- Task 2/index.js
+++ Task 2/index.js
@@ -1,9 +1,14 @@
-let quote = "You miss 100% of the shots you don’t take.–Wayne Gretzky" ;
+let quote = "\"You miss 100% of the shots you don’t take.–Wayne Gretzky\" " ;
 console.log(quote);
 
+
+
+
+
 function numberOf(){
-    let str = 'these biscuits are lovely' ;
-    let lenth= str.split(' ').length;
-    console.log(lenth);
+    let str = 'these biscuits are sweet and lovely' ;
+    let count =  str.split(' ').length;
+    console.log(count);
+    
 }
 numberOf();
