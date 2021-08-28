74c1c58ce5c8206d093de2e9c3dd21647453824f
PrincessMaggy <edoziemagdalene@gmail.com>
2021-08-27 18:03:04 -0700

    Adjusted the color number to 3 digit

--- Task 4/index.js
+++ Task 4/index.js
@@ -4,6 +4,9 @@ let col = document.querySelector('span');
 const change =() =>{
     
 let guess =Math.floor((Math.random()*255));
+while ((guess+"").length < 3){
+    guess= '0'+ guess;
+}
 document.body.style.backgroundColor = '#'+ guess;
 col.innerHTML = "#"+ guess;
 }
